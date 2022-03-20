9ec5d926bc92d49d0471813c74a17583d0752656
dohyeon5626 <kodohyeon71@gmail.com>
2022-03-21 00:07:52 +0900

    📑 :: help, cls 명령어 로직 추가
    help, cls 명령어 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
 import './App.css'
+import CliInput from "./components/CliInput";
+import CliMessage from "./components/CliMessage";
+import Help from './components/Help'
 
 function App() {
-  return
+  const [value, setValue] = useState([<CliMessage message = {[{text: "<Dohyeon5626:\\Portfolio> help", color: "#FFFFFF"}]}></CliMessage>, <Help></Help>]); // eslint-disable-line no-unused-vars
+  return (<>
+    <div>{value}</div>
+    <CliInput addStack = {(c) => setValue(new Array(value, c))} clearStack = {() => setValue([])}></CliInput>
+  </>)
 }
 
 export default App
--- src/components/CliInput.js
+++ src/components/CliInput.js
@@ -1,16 +1,46 @@
+import React, { useState } from "react";
 import CliMessage from "./CliMessage"
 import './style/CliInput.css'
+import Help from './Help'
 
-function CliInput() {
+function CliInput(props) {
+    const [inputValue, setInputValue] = useState("")
+
+    const handleUserInput = (e) => {
+      setInputValue(e.target.value)
+    }
+  
+    const resetInputField = () => {
+      setInputValue("")
+    }
+
+    const addStack = (e, r) => {
+        if (e.key === "Enter") {
+            if (r === "help" || r === "HELP") {
+                props.addStack(
+                    <>
+                        <CliMessage message = {[{text: "<Dohyeon5626:\Portfolio> " + r, color: "#FFFFFF"}]}></CliMessage>
+                        <Help></Help>
+                    </>
+                )
+            }
+            if (r === "cls" || r === "CLS") {
+                props.clearStack()
+            }
+            resetInputField()
+        }
+    }
+    
     const position = [
         {
             text: "<Dohyeon5626:\\Portfolio>",
             color: "#FFFFFF"
         }
     ]
+    
     return (<div className="line">
             <CliMessage message={position}></CliMessage>
-            <input className="command"></input>
+            <input className="command" value={inputValue} onChange={handleUserInput} onKeyPress = {(e) => addStack(e, e.target.value)}></input>
         </div>)
 }
 
